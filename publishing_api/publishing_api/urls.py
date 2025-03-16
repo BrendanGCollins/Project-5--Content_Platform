866974af0009561b971c1fbbb6f69b87bb120c93
Brendan <brendancollins89@hotmail.com>
2025-03-16 18:04:22 +0000

    feat: include articles URLs in main project URL configuration

--- publishing_api/publishing_api/urls.py
+++ publishing_api/publishing_api/urls.py
@@ -1,22 +1,15 @@
-"""
-URL configuration for publishing_api project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+# Import the admin site for the Django admin interface.
 from django.contrib import admin
-from django.urls import path
 
+# Import 'path' and 'include' to create URLs and include other URL files.
+from django.urls import path, include
+
+# Define the URL patterns for the project.
 urlpatterns = [
+    # The admin site is available at '/admin/'.
     path('admin/', admin.site.urls),
+    
+    # Include the URL patterns from the articles app under '/api/'.
+    # This means that any URL starting with 'api/' will be handled by the articles app URLs.
+    path('api/', include('articles.urls')),
 ]
