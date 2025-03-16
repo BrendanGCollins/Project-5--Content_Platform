e85a01afa7675e4690e9a78e58c612aae616d536
Brendan <brendancollins89@hotmail.com>
2025-03-16 22:59:02 +0000

    feat: register Article model in Django admin

--- publishing_api/articles/admin.py
+++ publishing_api/articles/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Article
 
-# Register your models here.
+# Register the Article model so it shows up in the Django admin.
+admin.site.register(Article)
