65584f41c9c481581fb6827100259f1292da2d89
Brendan <brendancollins89@hotmail.com>
2025-03-16 18:11:36 +0000

    feat: add default homepage view in articles/views.py

--- publishing_api/articles/views.py
+++ publishing_api/articles/views.py
@@ -1,6 +1,9 @@
 # Import generics from Django REST Framework.
 from rest_framework import generics
 
+# Import HttpResponse to return a simple text response for the homepage.
+from django.http import HttpResponse
+
 # Import the Article model from the current app's models.
 from .models import Article
 
@@ -8,6 +11,11 @@ from .models import Article
 from .serializers import ArticleSerializer
 
 # Define the ArticleListCreateAPIView class.
+# This view handles listing all articles and creating new ones.
 class ArticleListCreateAPIView(generics.ListCreateAPIView):
     queryset = Article.objects.all().order_by('-created_at')
     serializer_class = ArticleSerializer
+
+# Define a simple home view for the default homepage.
+def home(request):
+    return HttpResponse("Welcome to the Community Publishing Platform API!")
