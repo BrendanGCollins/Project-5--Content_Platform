1d8bc499a4cf117144ff021b54ccf0ed1d4d1278
Brendan <brendancollins89@hotmail.com>
2025-03-17 14:50:09 +0000

    feat: add delete button in ArticleList to remove articles

--- my-app/src/components/ArticleList.js
+++ my-app/src/components/ArticleList.js
@@ -1,17 +1,23 @@
 import React from 'react';
 
-// Component to display a list of articles
-const ArticleList = ({ articles }) => {
+// Component to display a list of articles.
+// Props:
+//   articles: Array of article objects.
+//   onDelete: Function to call when an article needs to be deleted.
+const ArticleList = ({ articles, onDelete }) => {
+  // If there are no articles, display a message.
   if (!articles || articles.length === 0) {
     return <p>No articles available.</p>;
   }
-  
+
   return (
     <div>
       {articles.map(article => (
         <div key={article.id} className="article">
           <h2>{article.title}</h2>
           <p>{article.content}</p>
+          {/* Delete button: calls onDelete with the article id */}
+          <button onClick={() => onDelete(article.id)}>Delete</button>
         </div>
       ))}
     </div>
